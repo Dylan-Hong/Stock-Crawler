59dc4f4bf127c4be7b2a93970d7d99c20564e128
Dylan-Hong <71960884+Dylan-Hong@users.noreply.github.com>
2021-07-13 23:15:06 +0800

    Update SystematicInvest.py

--- SystematicInvest.py
+++ SystematicInvest.py
@@ -1,11 +1,12 @@
-# GitTest
 import time
 import random
 import math
 import pandas as pd
 import requests
-import Function_def as Func
-
+import Function_def as func
+# 待完成
+# 1.手續費跟交易稅
+# 2.支援多個月
 # 建立一個class儲存當天的資訊
 class PriceInDay:
     # 建構子包含日期、收盤價
@@ -19,24 +20,19 @@ class PriceInDay:
 # -------------------------------------------------------------------------------
 # 設定日期
 StartYear = 2020
-StartMonth = 5
+StartMonth = 1
 EndYear = 2020
-EndMonth = 6
+EndMonth = 5
 # 設定標的
 TargetStockNo = '0050'
 # 投資頻率與金額設定
 InvestFreq = 1
 InvestAmount = 10000
-
-# 固定參數
-FeeRate = 0.001425
-TaxRate = 0.003
 # -------------------------------------------------------------------------------
 
 # 檔案名稱
-FileName = TargetStockNo +'_'+ Func.SetTimeString( StartYear, StartMonth ) + 'To' + Func.SetTimeString( EndYear, EndMonth )
+FileName = TargetStockNo +'_'+ func.SetTimeString( StartYear, StartMonth ) + 'To' + func.SetTimeString( EndYear, EndMonth )
 
-stop = 1
 YearCnt = MonthCnt = 0
 DelayTimeArray = [ 5, 6, 7, 4, 8 ]
 
@@ -45,6 +41,10 @@ HoldAmount = 0
 HoldQuantity = 0
 HoldCost = 0
 
+# 建立dataframe
+oFrame = pd.DataFrame( columns = [ '買進日期', '買進價格', '買進股數', '買進金額', '持股數量', '持股成本', '持股金額', '損益金額', '損益比例' ] )
+oFrameIndex = 0
+
 # 計算流程：讀每個月第一個交易日收盤價 -> 計算可買入股數 -> 設定平均成本 -> 設定資金大小 -> 設定目前損益
 while( 1 ):
 
@@ -52,9 +52,9 @@ while( 1 ):
     Year = StartYear + YearCnt
     Month = StartMonth + MonthCnt
     # 累加一個月份
-    [ YearCnt, MonthCnt, IsReachEnd ] = Func.AccumulateMonth( Year, Month, YearCnt, MonthCnt, EndYear, EndMonth )
+    [ YearCnt, MonthCnt, IsReachEnd ] = func.AccumulateMonth( Year, Month, YearCnt, MonthCnt, EndYear, EndMonth )
     # 將日期轉為字串格式
-    TargetDate = Func.SetTimeString( Year, Month )
+    TargetDate = func.SetTimeString( Year, Month )
     
     # 從網頁上讀資料
     # 設定路徑
@@ -68,7 +68,7 @@ while( 1 ):
     # 計算買進股數
     BuyQuantity = math.floor( InvestAmount / ClosePrice )
     # 計算買進金額
-    BuyAmount = math.ceil( ClosePrice * BuyQuantity * ( 1 + FeeRate ) )
+    BuyAmount = ClosePrice * BuyQuantity
     
     # 更新持股成本、數量
     # 持股數量
@@ -78,20 +78,21 @@ while( 1 ):
     # 持股均價
     HoldCost = HoldAmount / HoldQuantity
 
+    # 輸出資料：[ 買進日期、買進價格、買進股數、買進金額、持股數量、持股均價、持有金額、損益金額、損益比例 ]
 
-
-    # # 輸出資料：[ 買進日期、買進價格、買進股數、買進金額、持有成本、持有股數、損益金額、損益比例 ]
     # print( '買進日期 : ', data[ data.columns[ 0 ][ 0 ],  '日期' ][ 0 ] )
     # print( '買進價格 : ', ClosePrice )
     # print( '買進股數 : ', BuyQuantity )
     # print( '買進金額 : ', BuyAmount )
     # print( '持股數量 : ', HoldQuantity )
+    # 買進價格平均
     # print( '持股均價 : ', HoldCost )
     # print( '持有金額 : ', HoldAmount )
-    print( '損益金額 : ', ClosePrice * HoldQuantity * ( 1 + FeeRate + TaxRate ) - HoldAmount )
+    # print( '損益金額 : ', ( ClosePrice - HoldCost ) * HoldAmount )
     # print( '損益比例 : ', '{:.2%}'.format( ClosePrice / HoldCost - 1 ) )
     # print()
-
+    oFrame.loc[ oFrameIndex ] = [ data[ data.columns[ 0 ][ 0 ],  '日期' ][ 0 ], ClosePrice, BuyQuantity, BuyAmount, HoldQuantity, HoldCost, HoldAmount , ( ClosePrice - HoldCost ) * HoldAmount, '{:.2%}'.format( ClosePrice / HoldCost - 1 ) ]
+    oFrameIndex += 1
     time.sleep( random.choice( DelayTimeArray ) )
 
     # 檢查是否已經計算到end date
@@ -102,10 +103,10 @@ while( 1 ):
 # 存入PriceInDay的物件陣列中
 # Price0701 = PriceInDay( data.iat[ 0, 0 ], data.iat[ 0, 6 ] )
 
-# writer = pd.ExcelWriter( './practice/xlsfile/' + FileName + '.xlsx', engine='openpyxl' )
+writer = pd.ExcelWriter( './' + FileName + '.xlsx', engine='openpyxl' )
 
-# data.to_excel( writer, sheet_name = '2330' )
-# writer.save()
+oFrame.to_excel( writer, sheet_name = TargetStockNo )
+writer.save()
 
 """ 
 其他紀錄
