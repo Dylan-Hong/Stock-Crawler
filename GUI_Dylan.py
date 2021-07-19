d51b74a23897e5b42c39e6ac1e19a454f215bd4b
Dylan-Hong <71960884+Dylan-Hong@users.noreply.github.com>
2021-07-19 23:21:02 +0800

    新增要用place的code
    還沒完成，只寫了window跟項目

--- GUI_Dylan.py
+++ GUI_Dylan.py
@@ -2,82 +2,89 @@ import tkinter as tk
 from tkinter import ttk
 import math
 
-def calculate():
-    btnstr.set( 'hello' )
-window = tk.Tk()
-window.geometry('800x600')
-window.configure(background='white')
-
-def calculate_bmi_number():
-    height = float(height_entry.get()) / 100
-    weight = float(weight_entry.get())
-    print( height )
-    print( weight )
-    bmi_value = round(weight / math.pow(height, 2), 2)
-    result = '你的 BMI 指數為：{} {}'.format(bmi_value, get_bmi_status_description(bmi_value))
-    # 將計算結果更新到 result_label 文字內容
-    result_label.configure(text=result)
-
-def get_bmi_status_description(bmi_value):
-    if bmi_value < 18.5:
-        return '體重過輕囉，多吃點！'
-    elif bmi_value >= 18.5 and bmi_value < 24:
-        return '體重剛剛好，繼續保持！'
-    elif bmi_value >= 24 :
-        return '體重有點過重囉，少吃多運動！'
-
-# font設定字體跟大小
-# bg設定背景顏色
-# fg設定字體顏色
-# row：定義這個部件坐落在哪一列，如row=2
-# column：定義這個部件坐落在哪一欄，如column=2
-# rowspan：定義這個部件跨幾列，如rowspan=2
-# columnspan：定義這個部件跨幾欄，如columnspan=2
-# padx､pady：定義部件與cell邊框的橫縱向的距離，如padx=10
-# ipadx､ipady：定義部件與部件間橫縱向的距離，如ipadx=5
-# label
-label1 = tk.Label( window, text = "帳號", font = ('Arial', 12 ), bg = 'yellow', fg = 'black' )
-label1.grid( row = 0, column = 0 )
-label2 = tk.Label( window, text = "密碼", font = ( '', 12 ), bg = 'gray', fg = 'blue' )
-label2.grid( row = 1, column = 0 )
-
-# entry
-height_entry = tk.Entry( window )
-height_entry.grid( row = 0, column = 1 )
-weight_entry = tk.Entry( window )
-weight_entry.grid( row = 1, column = 1 )
-
-# button
-button1 = tk.Button( window, text = '登    入', bg = 'blue',activebackground = 'RED', font = 8, bd = 4, command = calculate_bmi_number )
-button1.grid( row = 2, column = 0, columnspan = 2, sticky = 'we' )
-button2 = tk.Button( window, text = '加入會員', bg = 'blue',activebackground = 'RED', font = 8, bd = 4 )
-button2.grid( row = 2, column = 2, columnspan = 1, sticky = 'we' )
-button3 = tk.Button( window, text = '與我聯繫', bg = 'blue',activebackground = 'RED', font = 8, bd = 4 )
-button3.grid( row = 2, column = 3, columnspan = 1, sticky = 'we' )
-
-btnstr = tk.StringVar() # 初始化tk的字串變數
-btnstr.set( '按看看' )
-btn = tk.Button( window, bg='violet', fg='black', textvariable=btnstr, font=('微軟正黑體', 20), command = calculate )
-btn.grid( row = 2, column = 4 )
-
-result_label = tk.Label(window)
-result_label.grid( row = 3, column = 3, columnspan = 1, sticky = 'we' )
-
-# http://andrewpythonarduino.blogspot.com/2018/04/python_6.html
+""" 
+這邊練習用place
+ """
+# window = tk.Tk()
+# window.geometry('800x600')
+# window.configure(background='white')
+ # http://andrewpythonarduino.blogspot.com/2018/04/python_6.html
 # ComboBox
-array = [ 1, 2, 3, 4 ]
-comGrade = ttk.Combobox( window, width=50, values=array )
-comGrade.grid( row = 4, column = 3 )
+
 # Radiobutton 
 # Checkbutton
 # Listbox 
 
 
-window.mainloop()
+""" 
+這邊練習用grid，並且有用函數執行button
+ """
+# def calculate():
+#     btnstr.set( 'hello' )
+# window = tk.Tk()
+# window.geometry('800x600')
+# window.configure(background='white')
 
-# """ 
-# 這個看懂了，不過用pack感覺比較不好用
-#  """
+# def calculate_bmi_number():
+#     height = float(height_entry.get()) / 100
+#     weight = float(weight_entry.get())
+#     print( height )
+#     print( weight )
+#     bmi_value = round(weight / math.pow(height, 2), 2)
+#     result = '你的 BMI 指數為：{} {}'.format(bmi_value, get_bmi_status_description(bmi_value))
+#     # 將計算結果更新到 result_label 文字內容
+#     result_label.configure(text=result)
+
+# def get_bmi_status_description(bmi_value):
+#     if bmi_value < 18.5:
+#         return '體重過輕囉，多吃點！'
+#     elif bmi_value >= 18.5 and bmi_value < 24:
+#         return '體重剛剛好，繼續保持！'
+#     elif bmi_value >= 24 :
+#         return '體重有點過重囉，少吃多運動！'
+
+# # font設定字體跟大小
+# # bg設定背景顏色
+# # fg設定字體顏色
+# # row：定義這個部件坐落在哪一列，如row=2
+# # column：定義這個部件坐落在哪一欄，如column=2
+# # rowspan：定義這個部件跨幾列，如rowspan=2
+# # columnspan：定義這個部件跨幾欄，如columnspan=2
+# # padx､pady：定義部件與cell邊框的橫縱向的距離，如padx=10
+# # ipadx､ipady：定義部件與部件間橫縱向的距離，如ipadx=5
+# # label
+# label1 = tk.Label( window, text = "帳號", font = ('Arial', 12 ), bg = 'yellow', fg = 'black' )
+# label1.grid( row = 0, column = 0 )
+# label2 = tk.Label( window, text = "密碼", font = ( '', 12 ), bg = 'gray', fg = 'blue' )
+# label2.grid( row = 1, column = 0 )
+
+# # entry
+# height_entry = tk.Entry( window )
+# height_entry.grid( row = 0, column = 1 )
+# weight_entry = tk.Entry( window )
+# weight_entry.grid( row = 1, column = 1 )
+
+# # button
+# button1 = tk.Button( window, text = '登    入', bg = 'blue',activebackground = 'RED', font = 8, bd = 4, command = calculate_bmi_number )
+# button1.grid( row = 2, column = 0, columnspan = 2, sticky = 'we' )
+# button2 = tk.Button( window, text = '加入會員', bg = 'blue',activebackground = 'RED', font = 8, bd = 4 )
+# button2.grid( row = 2, column = 2, columnspan = 1, sticky = 'we' )
+# button3 = tk.Button( window, text = '與我聯繫', bg = 'blue',activebackground = 'RED', font = 8, bd = 4 )
+# button3.grid( row = 2, column = 3, columnspan = 1, sticky = 'we' )
+
+# btnstr = tk.StringVar() # 初始化tk的字串變數
+# btnstr.set( '按看看' )
+# btn = tk.Button( window, bg='violet', fg='black', textvariable=btnstr, font=('微軟正黑體', 20), command = calculate )
+# btn.grid( row = 2, column = 4 )
+
+# result_label = tk.Label(window)
+# result_label.grid( row = 3, column = 3, columnspan = 1, sticky = 'we' )
+
+# window.mainloop()
+
+""" 
+這個看懂了，不過用pack感覺比較不好用
+ """
 # # 這個步驟建立一個主視窗
 # window = tk.Tk()
 # # 設定視窗標題、大小和背景顏色
@@ -109,14 +116,14 @@ window.mainloop()
 # # window2.mainloop()
 
 
-# """ 
-# 1.繼承tk.frame
-# 2.master不知道幹嘛的
-# 3.標籤使用tk.Label
-# 4.按鈕使用tk.Button
-# 5.mbox.messagebox
-# 網址：https://www.itread01.com/content/1552777204.html
-#  """
+""" 
+1.繼承tk.frame
+2.master不知道幹嘛的
+3.標籤使用tk.Label
+4.按鈕使用tk.Button
+5.mbox.messagebox
+網址：https://www.itread01.com/content/1552777204.html
+ """
 # import tkinter as tk
 # import tkinter.messagebox as mbox
 
